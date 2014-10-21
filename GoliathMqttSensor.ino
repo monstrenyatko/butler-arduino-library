e1bff356943611f3df9dc6a11a4c3a97a1918cae
Oleg Kovalenko <monstrenyatko@gmail.com>
2014-10-21 12:54:09 +0300

    Add 'ino' file to allow build from Arduino IDE

---
+++ GoliathMqttSensor.ino
@@ -0,0 +1,7 @@
+/* Stub to allow build from Arduino IDE */
+/* Includes to external libraries */
+#include <MemoryFree.h>
+#include <Countdown.h>
+#include <MQTTClient.h>
+#include <SoftwareSerial.h>
+
