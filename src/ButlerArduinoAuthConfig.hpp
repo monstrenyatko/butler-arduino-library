8808efeacdf6f68356ef926ec8f68265217b2c31
Oleg Kovalenko <monstrenyatko@gmail.com>
2017-08-04 00:57:52 -0500

    Add `Auth` configuration.

---
+++ src/ButlerArduinoAuthConfig.hpp
@@ -0,0 +1,92 @@
+/*
+ *******************************************************************************
+ *
+ * Purpose: Authentication configuration structure.
+ *
+ *******************************************************************************
+ * Copyright Oleg Kovalenko 2017.
+ *
+ * Distributed under the MIT License.
+ * (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *******************************************************************************
+ */
+
+#ifndef BUTLER_ARDUINO_AUTH_CONFIG_H_
+#define BUTLER_ARDUINO_AUTH_CONFIG_H_
+
+/* System Includes */
+#include <WString.h>
+/* Internal Includes */
+
+
+namespace Butler {
+namespace Arduino {
+
+namespace Config {
+
+template<uint8_t FINGERPRINTS_QTY = 2>
+struct AuthConfig {
+	bool											paired = false;
+	String											fingerprints[FINGERPRINTS_QTY];
+
+	AuthConfig() {
+		for (uint8_t i = 0; i < getMaxFingerprintsQty(); ++i) {
+			fingerprints[i] = String();
+		}
+	}
+
+	void set(const AuthConfig &o) {
+		paired = o.paired;
+		for (uint8_t i = 0; i < getMaxFingerprintsQty(); ++i) {
+			if (i < o.getMaxFingerprintsQty()) {
+				fingerprints[i] = o.fingerprints[i];
+			} else {
+				fingerprints[i] = String();
+			}
+		}
+	}
+
+	bool isValid() const {
+		return true;
+	}
+
+	bool isEqual(const AuthConfig &o) const {
+		bool res = (paired == o.paired);
+		res = res && (getMaxFingerprintsQty() == o.getMaxFingerprintsQty());
+		for (uint8_t i = 0; res && i < getMaxFingerprintsQty(); ++i) {
+			res = fingerprints[i].equals(o.fingerprints[i]);
+		}
+		return res;
+	}
+
+	uint8_t getMaxFingerprintsQty() const {
+		return FINGERPRINTS_QTY;
+	}
+
+	uint8_t getFingerprintsQty() const {
+		uint8_t res = 0;
+		for (uint8_t i = 0; i < getMaxFingerprintsQty(); ++i) {
+			if (fingerprints[i].length()) {
+				++res;
+			}
+		}
+		return res;
+	}
+
+	bool resetFingerprints(uint8_t startIdx = 0) {
+		bool updated = false;
+		for (uint8_t i = startIdx; i < getMaxFingerprintsQty(); ++i) {
+			if (fingerprints[i].length()) {
+				fingerprints[i] = String();
+				updated = true;
+			}
+		}
+		return updated;
+	}
+};
+
+} // Config
+
+}}
+
+#endif // BUTLER_ARDUINO_AUTH_CONFIG_H_
---
+++ src/ButlerArduinoAuthJsonConfig.hpp
@@ -0,0 +1,91 @@
+/*
+ *******************************************************************************
+ *
+ * Purpose: Authentication JSON configuration structure.
+ *
+ *******************************************************************************
+ * Copyright Oleg Kovalenko 2017.
+ *
+ * Distributed under the MIT License.
+ * (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *******************************************************************************
+ */
+
+#ifndef BUTLER_ARDUINO_AUTH_JSON_CONFIG_H_
+#define BUTLER_ARDUINO_AUTH_JSON_CONFIG_H_
+
+/* System Includes */
+#include <WString.h>
+/* Internal Includes */
+#include "ButlerArduinoJsonConfig.hpp"
+#include "ButlerArduinoAuthConfig.hpp"
+
+
+namespace Butler {
+namespace Arduino {
+
+namespace Config {
+
+struct AuthJsonConfig: public JsonConfigNode, public AuthConfig<> {
+
+	bool decode(JsonObject &json) {
+		bool updated = false;
+		// PAIRED
+		if (json.containsKey(Strings::PAIRED)) {
+			bool v = json[Strings::PAIRED];
+			if (paired != v) {
+				paired = v;
+				updated = true;
+			}
+		}
+		// FINGERPRINTS
+		{
+			JsonArray &o = json[Strings::FINGERPRINTS];
+			if (JsonArray::invalid() != o) {
+				uint8_t idx = 0;
+				// Set new values
+				for(JsonArray::iterator it = o.begin();
+						idx < getMaxFingerprintsQty() && it != o.end();
+						++it)
+				{
+					const char *value = *it;
+					if (value) {
+						if (!fingerprints[idx].equals(value)) {
+							fingerprints[idx] = value;
+							updated = true;
+						}
+						++idx;
+					}
+				}
+				// Reset empty slots
+				updated = resetFingerprints(idx) || updated;
+			}
+		}
+		return updated;
+	}
+
+	void encode(JsonObject &json, JsonBuffer &jsonBuffer) {
+		// PAIRED
+		json[Strings::PAIRED] = paired;
+		// FINGERPRINTS
+		if (getFingerprintsQty()) {
+			JsonArray &o = jsonBuffer.createArray();
+			for (uint8_t i = 0; i < getMaxFingerprintsQty(); ++i) {
+				if (fingerprints[i].length()) {
+					o.add(fingerprints[i].c_str());
+				}
+			}
+			json[Strings::FINGERPRINTS] = o;
+		}
+	}
+
+	bool isValid() const {
+		return AuthConfig::isValid();
+	}
+};
+
+} // Config
+
+}}
+
+#endif // BUTLER_ARDUINO_AUTH_JSON_CONFIG_H_
